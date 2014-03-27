5293c2aab61bd2040c401c76d324ad281915b37b
jonathanjanssens <jonathan.janssens@gmail.com>
2014-03-27 18:22:47 +0000

    fixing request formats

--- serialRead.py
+++ serialRead.py
@@ -5,6 +5,7 @@ class serialRead:
     def __init__(self):
         self.serialPort = '/dev/tty.usbmodemfd121'
         self.logFileLocation = 'output.log'
+        self.apiUrl = 'http://projects.jjw.al/hotwire/service/'
         if '--batch' in sys.argv:
             self.sendLog()
             sys.exit()
@@ -19,9 +20,8 @@ class serialRead:
 
     def sendLog(self):
         print 'processing batch'
-        url = 'http://projects.jjw.al/hotwire/service/'
-        payload = {'file': open(self.logFileLocation, 'rb'), 'datatype': 'csv'}
-        r.requests.post(url, files=logFile)
+        payload = {'file': open(self.logFileLocation, 'rb')}
+        r.requests.post('%s?datatype=csv' % self.apiUrl, files=logFile)
         print 'done'
 
     def checkSerial(self):
@@ -37,19 +37,21 @@ class serialRead:
 
     def prepareLog(self):
         with open(self.logFileLocation, 'w') as f:
-            fields = '"s01","s02","s03","timestamp"\n'
+            fields = '"user_id","sensor_id","value","timestamp"\n'
             f.write(fields)
             f.flush
 
     def writeToLog(self):
         with open(self.logFileLocation, 'a+') as f:
-            data = '%s,%s,%s,%s\n' % (self.jsonIn['readings'][0]['value'], self.jsonIn['readings'][1]['value'], self.jsonIn['readings'][2]['value'], self.jsonIn['timestamp'])
-            f.write(data)
-            f.flush()
+            for reading in self.jsonIn['readings']:
+                data = '%s,%s,%s,%s\n' % (self.jsonIn['userId'], reading['sensorId'], reading['value'], self.jsonIn['timestamp'])
+                f.write(data)
+                f.flush()
 
     def apiCall(self):
-        data = '?s01=%s&s02=%s&s03=%s&timestamp=%s' % (self.jsonIn['readings'][0]['value'], self.jsonIn['readings'][1]['value'], self.jsonIn['readings'][2]['value'], self.jsonIn['timestamp'])
-        print data
+        for reading in self.jsonIn['readings']:
+            payload = {'user_id': self.jsonIn['user_id'], 'sensor_id': reading['sensor_id'], 'value': reading['value'], 'timestamp': self.jsonIn['timestamp']}
+            r.requests.post(self.apiUrl, params=payload)
 
     def printOut(self):
         print 'head:', self.jsonIn['readings'][0]['value']
